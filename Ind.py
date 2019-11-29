fe8735c479797748e94516fa4466a77cc26df497
ArChrisVa <chrisarv1237@gmail.com>
2019-11-29 17:45:40 +0200

    ignore commented code , creates index in ram

--- Ind.py
+++ Ind.py
@@ -1,5 +1,6 @@
 import pymongo
 from pymongo import MongoClient
+from collections import defaultdict
 
 
 class Index:
@@ -10,8 +11,15 @@ class Index:
         self.cluster = MongoClient("mongodb+srv://chris:12340987@cluster0-i10z0.azure.mongodb.net/test?retryWrites=true&w=majority")
         self.db = self.cluster["InformationRetrieval"]
         self.collection = self.db["Indexer"]
-
+        self.doc_names = self.db["doc_names"]
+        self.di = {}
+        self.collection.delete_many({})
     def update_indexer(self, data):
+
+        #gemizei to di ,lockaroun ta threads, travaei olo to data i vasi ,adeiazei to di
+
+        self.create_inverted_index_in_ram(data)
+        x = 0 # tha gurnaei pisw enan aritmo oste na arxizei apo ekei to kainourgio coubt , episis borei na dimiourgoude pedia xoris na einai embedded
         '''
 
         Parameters:
@@ -21,6 +29,7 @@ class Index:
         words_in_list : λίστα που περιέχει τις λέξεις ενός εγγράφου
         Στην αρχή γίνεται μια απλή επεξεργασία της λίστας data για να διαμορφωθούν οι titles,content.Αποτελεί προσωρινή υλοποίηση
 
+        '''
         '''
         titles = []
         content = []
@@ -36,16 +45,19 @@ class Index:
             words_in_list.append(li)
 
         '''
+        '''
         Paramater:
         -----------------------------------------
         list_of_terms : κάθε φορα που τρέχει το πρόγραμμα τραβάει από τη βάση όλους τους όρους και τους αποθηκεύει στη list_of_terms'''
 
         list_of_terms = list(self.collection.find().distinct("_id"))
-
+        list_of_document_names = list(self.doc_names.find().distinct("_id"))
         '''debugging purposes'''
 
         #self.collection.delete_many({})
-
+        # x = self.collection.find({"_id" : "information"})
+        # for i in x:
+        #     print(i)
         '''
         Ενημέρωση του ανεστραμμένου καταλόγου
         
@@ -61,33 +73,142 @@ class Index:
             Χρειάζεται για να ξέρουμε αν ένας όρος υπάρχει ήδη μια φορά σε ένα έγγραφο , ώστε να μην αυξηθεί το @counter,
             αλλά μόνο να προσθέσουμε τη θέση στην οποία ξαναβρέθηκε
         '''
-        for i, title in enumerate(titles):
-            for pos, word in enumerate(words_in_list[i]):
-                post = {"_id": word, "counter": 1, "docPos": [(title, pos)]}
-                if word in list_of_terms:
-                    flag = False
-                    word_document_titles = list(self.collection.find({"_id":word}).distinct("docPos"))
-                    for t in word_document_titles:
-                        if title == t[0]:
-                            flag = True
-                            break
-                    if flag:
-                        self.collection.update_one({"_id": word},
-                                               {"$addToSet": {"docPos": (title, pos)}})
-                    else:
-                        self.collection.update_one({"_id": word},
-                                               {"$inc": {"counter": 1}, "$addToSet": {"docPos": (title, pos)}})
-                else:
-                    self.collection.insert_one(post)
-                    list_of_terms.append(word)
 
+        for i, title in enumerate(self.di.keys(),x):
+            found = []
+            if title not in list_of_document_names:
+                list_of_document_names.append(title)
+                li = list(self.di.get(title))
+                self.doc_names.insert_one({"_id" : title, "terms":li})
+            for pos, word in enumerate(list(self.di.get(title))):
+                post = ({"_id": word,
+                        "counter": 1,
+                        "docPos": [{
+                            "nameDoc":title,
+                            "tf":1,
+                            "positions":[pos]
+                        }]})
+                # print(post.get("counter"))
+                # if word in list_of_terms:
+                #     flag = True
+                #     if word in found:
+                #        flag = False
+                #     else:
+                #        found.append(word)
+                #     a = "docPos." + str(i) + ".positions"
+                #     b = "docPos." + str(i) + ".tf"
+                #     if flag:
+                #         self.collection.update_one({"_id": word ,"docPos.nameDoc" : title},{"$inc" : {b : 1}})   #ftiakse ta insert na doulevoun sosta
+                #         self.collection.update_one({"_id" : word,"docPos.nameDoc" : title},{"$push" : {a : pos}})
+                #     else:
+                #         self.collection.aggregate([
+                #             {
+                #                 "$addFields":{
+                #                     "nameDoc": title,
+                #                     "tf": 1,
+                #                     "positions": [pos]
+                #                 }
+                #             }
+                #         ])
+                #         self.collection.update_one({"_id": word, "docPos.nameDoc": title}, {"$inc": {b : 1}})
+                #         self.collection.update_one({"_id": word },{"$inc" : {"counter" : 1}})
+                #         self.collection.update_one({"_id" : word,"docPos.nameDoc" : title},{"$push" : {a : pos }})
+                # else:
+                #     self.collection.insert_one(post)
+                #     list_of_terms.append(word)
+
+    #successfully creates inverted index in ram
+    def create_inverted_index_in_ram(self,data):
+        titles = []
+        content = []
+        for i, line in enumerate(data):
+            if i % 2 == 0:
+                titles.append(line)
+            else:
+                content.append(line)
+
+        words_in_list = []
+        for phrase in content:
+            li = phrase.split(" ")
+            words_in_list.append(li)
+
+        for i in range(len(titles)):
+            self.di.update({titles[i] : words_in_list[i]})
+
+        # print(self.di)
+        # post = ({"_id": word,
+        #          "counter": 1,
+        #          "docPos": [{
+        #              "nameDoc": title,
+        #              "tf": 1,
+        #              "positions": [pos]
+        #          }]})
+        alist = []
+        found = []
+        posts = []
+        for title in titles:
+            s = []
+            d = defaultdict(list)
+            for i,word in enumerate(self.di.get(title)):
+                s.append((word,i))
+            for k,v in s:
+                d[k].append(v)
+            alist.append(d)
+            # for word in (self.di.get(title)):
+            #     print(d.get(word))
+            # embeddedDict = {
+            #                  "nameDoc": title,
+            #                  "tf": len(d.get(word)),
+            #                  "positions": d.get(word)
+            #              }
+            for word in(self.di.get(title)):
+                embeddedDict = {
+                    "nameDoc": title,
+                    "tf": len(d.get(word)),
+                    "positions": d.get(word)
+                }
+                if word not in found:
+                    post = ({"_id": word,
+                             "counter": 1,
+                             "docPos": [embeddedDict]})
+                    posts.append(post)
+                    found.append(word)
+                else:
+                    for pos in posts:
+                       namedocs = []
+                       if pos.get("_id") == word:
+                            x = pos.get("docPos",{})
+                            for y in x:
+                                namedocs.append(y.get("nameDoc"))
+                            if embeddedDict.get("nameDoc") not in namedocs:
+                                x.append(embeddedDict)
+
+            # for x in posts:
+            #     print(x.get("docPos",{})[0].get("positions"))
+
+        for x in posts:
+            print(x)
+
+        # for item in alist:
+        #     print(item.items())
+
+    def top_k_documents(self,query):
+        C = []
+        list_of_terms = list(self.collection.find().distinct("_id"))
+        for term in query:
+            if term in list_of_terms:
+                nt = self.collection.find({"_id":term}).distinct("counter")
+                N = self.collection.count_documents({})
+                print(N)
 
 with open("document.txt", "r") as doc:
+    #different documents
     data = doc.readlines()
 
 ind = Index()
 ind.update_indexer(data)
-
+#query = ["information","retrieval","and"]
+#ind.top_k_documents(query)
 
 '''
 
