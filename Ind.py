884b4a849ad8da15ae2bc02d79d9dd9b0739825a
ArChrisVa <chrisarv1237@gmail.com>
2019-12-02 20:12:06 +0200

    1 threaded crawler + indexer communicate and upÂ¨date database tasks : multithreaded indexer + sorting

--- Ind.py
+++ Ind.py
@@ -1,6 +1,5 @@
 import pymongo
 from pymongo import MongoClient
-from collections import defaultdict
 from numpy import log
 
 #updateMiniIndex-> locks only for append
@@ -9,51 +8,44 @@ from numpy import log
 class Index:
 
     '''Σύνδεση με MongoDB'''
-
+    # word_queue, word_queue_lock
     def __init__(self, word_queue, word_queue_lock):
         self.cluster = MongoClient("mongodb+srv://chris:12340987@cluster0-i10z0.azure.mongodb.net/test?retryWrites=true&w=majority")
         self.db = self.cluster["InformationRetrieval"]
         self.collection = self.db["Indexer"]
-        self.alist = []
         self.found = []
         self.miniIndex = []
-        self.numberOfCurrentDocumentsToBeUpdated = 0
-        # self.collection.delete_many({})
+        self.nofExtraDocs = 0
+        self.word_queue = word_queue
+        self.word_queue_lock = word_queue_lock
+        self.collection.delete_many({})
 
     def updateIndex(self):
 
-
-
-
-        '''
-        Paramater:
-        -----------------------------------------
-        list_of_terms : κάθε φορα που τρέχει το πρόγραμμα τραβάει από τη βάση όλους τους όρους και τους αποθηκεύει στη list_of_terms
-        '''
-
         list_of_terms = list(self.collection.find().distinct("_id"))
+
         '''
         Ενημέρωση του ανεστραμμένου καταλόγου
         '''
-
-        for x in to_be_updated: #allagi
-            term = x.get("_id")
+        # lock
+        for post in self.miniIndex:
+            term = post.get("_id")
             if term in list_of_terms:
-                list_to_append = x.get("docPos")
+                list_to_append = post.get("docPos")
                 self.collection.update_one({"_id":term},{"$inc" : {"counter":len(list_to_append)}})
                 for doc in list_to_append:
                     self.collection.update_one({"_id":term},{"$push" :{"docPos":doc}})
             else:
                 list_of_terms.append(term)
-                self.collection.insert_one(x)
+                self.collection.insert_one(post)
 
         #update for N of IDF
-        self.collection.update_one({"_id": "NumOfDocumentsInBase"}, {"$inc": {"count": self.numberOfCurrentDocumentsToBeUpdated}},upsert=True)
+        self.collection.update_one({"_id": "NumOfDocumentsInBase"}, {"$inc": {"count": self.nofExtraDocs}},upsert=True)
 
-        self.posts.clear()
-        self.alist.clear()
+        self.miniIndex.clear()
         self.found.clear()
-        self.numberOfCurrentDocumentsToBeUpdated = 0
+        self.nofExtraDocs = 0
+        # lock
 
     def updateMiniIndex(self):
 
@@ -67,64 +59,46 @@ class Index:
         #append
         #release
 
-        content = []
-        for i, line in enumerate(data):
-            if i % 2 == 0:
-                title = line
-            else:
-                content.append(line)
+        #checks if word_queue is empty
+        if not self.word_queue:
+            pass
+        else:
 
-        words_in_list = []
-        for phrase in content:      #pop from word_queue
-            words_in_list = phrase.split(" ")
-
-        # for x in words_in_list:
-        #     print(x)
-
-
-
-        # print(self.di)
-        # print("=====================")
-        #creation of index ,will put comments later
-
-
-        s = []
-        d = defaultdict(list)
-        for i,word in enumerate(words_in_list):
-            s.append((word,i))
-        for k,v in s:
-            d[k].append(v)
-        self.alist.append(d)
-        for word in(words_in_list):
-            embeddedDict = {
-                "nameDoc": title,
-                "tf": len(d.get(word)),
-                "positions": d.get(word)
-            }
-            if word not in self.found:          #sorted
-                post = ({"_id": word,
-                         "counter": 1,
-                         "docPos": [embeddedDict]})
-                self.posts.append(post)
-                self.found.append(word)
-            else:
-                for pos in self.posts:
-                    if pos.get("_id") == word:
-                        namedocs = []
-                        x = pos.get("docPos", {})
-                        for y in x:
-                            namedocs.append(y.get("nameDoc"))
-                        if embeddedDict.get("nameDoc") not in namedocs:
-                            x.append(embeddedDict)
-                            pos["counter"] += 1
-
-        self.numberOfCurrentDocumentsToBeUpdated += 1
+            next = self.word_queue.pop(0)
+            title = next.get("link")
+            wordsInList = next.get("words")
+            # {term 1 : tf , term 2 : tf}
+            tfDict = dict((x, wordsInList.count(x)) for x in set(wordsInList))
+            for word in tfDict.keys():
+                embeddedDict = {
+                    "nameDoc": title,
+                    "tf": tfDict.get(word)
+                }
+                # found is a list that stores all words a crawler sends to miniIndex before each update to db
+                if word not in self.found:          #sorted # future commit
+                    miniIndexEntry = ({"_id": word,
+                                       "counter": 1,
+                                       "docPos": [embeddedDict]})
+                    self.miniIndex.append(miniIndexEntry)
+                    self.found.append(word)
+                else:
+                    for pos in self.miniIndex:
+                        if pos.get("_id") == word:
+                            namedocs = []
+                            x = pos.get("docPos", {})
+                            for y in x:
+                                namedocs.append(y.get("nameDoc"))
+                            if embeddedDict.get("nameDoc") not in namedocs:
+                                x.append(embeddedDict)
+                                pos["counter"] += 1
+            #update N for idf
+            self.nofExtraDocs += 1
 
     def print_posts(self):
-        for x in self.posts:
+        for x in self.miniIndex:
             print(x)
 
-    #top-k (in progress)
+    #top-k (slides)
     def top_k_documents(self,query):
         C = {}
         list_of_terms = list(self.collection.find().distinct("_id"))
@@ -146,30 +120,12 @@ class Index:
 
             #leipei kanonikopoihsh sth monada ousiastika ti einai to Ld
 
-# with open("document.txt", "r") as doc:
-#     #different documents
-#     data = doc.readlines()
-#
-# with open("document2.txt", "r") as doc:
-#     #different documents
-#     data2 = doc.readlines()
-#
-# with open("document3.txt", "r") as doc:
-#     #different documents
-#     data3 = doc.readlines()
-#
-# with open("document4.txt", "r") as doc:
-#     #different documents
-#     data4 = doc.readlines()
 
-#ind.top_k_documents(["the","ahahahha","and","company"])
-# ind.create_inverted_index_in_ram(data)
-# ind.create_inverted_index_in_ram(data3)
-# ind.update_indexer()
-# ind.create_inverted_index_in_ram(data2)
+# ind.top_k_documents(["the","ahahahha","and","company"])
+# ind = Index()
+# ind.updateIndex
 # ind.print_posts()
 # ind.update_indexer()
-# ind.create_inverted_index_in_ram(data4)
 # ind.print_posts()
 # ind.update_indexer()
 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import threading
-import Ind
-
+import time
+from Ind import Index
 from crawler import Crawler
 
+End = time.time() + 30
 
 Crawler.set_page_number(200)
 word_queue = []
@@ -12,18 +13,23 @@ word_queue_lock = threading.Lock()
 crawler1 = Crawler(word_queue, link_queue, word_queue_lock)
 # crawler1.addLink('https://www.geeksforgeeks.org/inverted-index/')
 crawler1.addLink('sample1.html')
-crawler2 = Crawler(word_queue, link_queue)
+crawler2 = Crawler(word_queue, link_queue, word_queue_lock)
 
 print('Starting threads')
 
 
 crawler1.start()
-crawler2.start()
+# crawler2.start()
 
-index = Ind(word_queue,word_queue_lock)
+ind = Index(word_queue,word_queue_lock)
+while time.time() < End:
+    ind.updateMiniIndex()
+    ind.print_posts()
+
+ind.updateIndex()
 
 crawler1.join()
-crawler2.join()
+# crawler2.join()
 
 
 print('Exiting main thread')
