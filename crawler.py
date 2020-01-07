8810edfb070b636431b0d02e681e921324a1e9d7
ArChrisVa <chrisarv1237@gmail.com>
2020-01-07 15:57:36 +0200

    evgala auta me to collision apo to lathos pou ekana

--- crawler.py
+++ crawler.py
@@ -44,15 +44,11 @@ class Crawler(threading.Thread):
             if len(Crawler.link_queue) == 0:
                 time.sleep(0.5)
             else:
-# <<<<<<< HEAD
-#                 print(Crawler.link_queue)
-#                 print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"8
-# =======
-                #print(Crawler.link_queue)
-# >>>>>>> a899d6710931d979ca4849a10085bcee77477c62
+
 
                 Crawler.link_queue_lock.acquire()
                 link = Crawler.link_queue.pop(0)
+                print(link)
                 Crawler.link_queue_lock.release()
                 extracted_words, extracted_links = html_extractor(link)  # crawl that link
                 Crawler.word_queue_lock.acquire()
@@ -63,22 +59,8 @@ class Crawler(threading.Thread):
                 Crawler.link_queue_lock.release()
                 Crawler.decrement_page_number()
 
-            # print(len(Crawler.word_queue))
+            print("number of items in crawler queue that wait to be processed by miniIndexPusher : " , len(Crawler.word_queue))
 
 
-
-
-# <<<<<<< HEAD
-            # if len(Crawler.word_queue) == 0:
-            #     time.sleep(0.5)
-            # else:
-            #     Crawler.word_queue_lock.acquire()
-            #     words = Crawler.word_queue.pop(0)
-            #     Crawler.word_queue_lock.release()
-
-            # send them for processing at index
-            # time.sleep(2)
-            #print(Crawler.nof_pages)
-# =======
             #time.sleep(2)
-# >>>>>>> a899d6710931d979ca4849a10085bcee77477c62
+
